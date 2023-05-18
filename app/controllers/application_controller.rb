0b8106db667bd1dc9467d0eaaaf55560bbec8715
Julia Wenger <julia.wenger@kellogg.northwestern.edu>
2023-05-18 02:26:04 +0000

    Final class update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # before_action :current_user
 
   # def current_user
-  #   puts "----- this code runs at the beginning of the request"
+     puts "----- this code runs at the beginning of the request"
+     @current_user = User.find_by({})
   # end
 end
